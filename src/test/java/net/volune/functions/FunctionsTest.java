4baacc40ac568a146e55d6870e5c73d36277a8b0
Volune <jeremy@volune.net>
2014-08-24 15:15:55 +0200

    test for BinaryOperator

--- src/test/java/net/volune/functions/FunctionsTest.java
+++ src/test/java/net/volune/functions/FunctionsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -14,22 +15,30 @@ public class FunctionsTest extends TestBase {
     @Test
     public void testBiFunctionBind1() throws Exception {
         Spy spy = new Spy();
-        map(Functions.bind1(spy::operate, 0));
+        map(Functions.bind1(spy::transform, 0));
         spy.checkConsumed(0, 1, 0, 2, 0, 3);
     }
 
     @Test
     public void testBiFunctionBind2() throws Exception {
         Spy spy = new Spy();
-        map(Functions.bind2(spy::operate, 0));
+        map(Functions.bind2(spy::transform, 0));
         spy.checkConsumed(1, 0, 2, 0, 3, 0);
     }
 
     @Test
     public void testBiFunctionBind12() throws Exception {
         Spy spy = new Spy();
-        generate(Functions.bind12(spy::operate, 8, 9));
+        generate(Functions.bind12(spy::transform, 8, 9));
         spy.checkConsumed(8, 9, 8, 9, 8, 9);
+    }
+
+    @Test
+    public void testBinaryOperatorBind12() throws Exception {
+        Spy spy = new Spy();
+        BinaryOperator<String> operator = spy::operate;
+        generate(Functions.bind12(operator, "a", "b"));
+        spy.checkConsumed("a", "b", "a", "b", "a", "b");
     }
 
     private void map(Function<Integer, String> function) {
--- src/test/java/net/volune/functions/Spy.java
+++ src/test/java/net/volune/functions/Spy.java
@@ -7,7 +7,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class Spy {
-    private List<Number> consumed = new ArrayList<>();
+    private List<Object> consumed = new ArrayList<>();
     private int resultGenerator = -1;
 
     public void consume(Number a1, Number a2) {
@@ -15,13 +15,19 @@ public class Spy {
         consumed.add(a2);
     }
 
-    public String operate(Number a1, Number a2) {
+    public String operate(String a1, String a2) {
         consumed.add(a1);
         consumed.add(a2);
         return Integer.toString(resultGenerator--, 10);
     }
 
-    public void checkConsumed(Number... expected) {
+    public String transform(Number a1, Number a2) {
+        consumed.add(a1);
+        consumed.add(a2);
+        return Integer.toString(resultGenerator--, 10);
+    }
+
+    public void checkConsumed(Object... expected) {
         assertEquals("Consumed arguments", Arrays.asList(expected), consumed);
     }
 }
