eca74d4f197773b180c8c8091936d21d25fef5e7
Volune <jeremy@volune.net>
2014-08-24 16:01:43 +0200

    Consumers applyXX functions to reorder arguments

--- src/main/java/net/volune/functions/Consumers.java
+++ src/main/java/net/volune/functions/Consumers.java
@@ -8,6 +8,22 @@ public class Consumers {
     private Consumers() {
     }
 
+    public static <T> BiConsumer<T, T> apply12(BiConsumer<T, T> consumer) {
+        return consumer;
+    }
+
+    public static <T> BiConsumer<T, T> apply21(BiConsumer<T, T> consumer) {
+        return (arg1, arg2) -> consumer.accept(arg2, arg1);
+    }
+
+    public static <T> BiConsumer<T, T> apply11(BiConsumer<T, T> consumer) {
+        return (arg1, arg2) -> consumer.accept(arg1, arg1);
+    }
+
+    public static <T> BiConsumer<T, T> apply22(BiConsumer<T, T> consumer) {
+        return (arg1, arg2) -> consumer.accept(arg2, arg2);
+    }
+
     public static <T, U, A1 extends T> Consumer<U> bind1(BiConsumer<T, U> consumer, A1 arg1) {
         return (arg2) -> consumer.accept(arg1, arg2);
     }
--- src/test/java/net/volune/functions/ConsumersTest.java
+++ src/test/java/net/volune/functions/ConsumersTest.java
@@ -2,9 +2,38 @@ package net.volune.functions;
 
 import org.junit.Test;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class ConsumersTest extends TestBase {
+    @Test
+    public void testBiConsumerApply12() throws Exception {
+        Spy spy = new Spy();
+        eachPair(Consumers.apply12(spy::consume));
+        spy.checkConsumed(1, 2, 3, 4, 5, 6);
+    }
+
+    @Test
+    public void testBiConsumerApply21() throws Exception {
+        Spy spy = new Spy();
+        eachPair(Consumers.apply21(spy::consume));
+        spy.checkConsumed(2, 1, 4, 3, 6, 5);
+    }
+
+    @Test
+    public void testBiConsumerApply11() throws Exception {
+        Spy spy = new Spy();
+        eachPair(Consumers.apply11(spy::consume));
+        spy.checkConsumed(1, 1, 3, 3, 5, 5);
+    }
+
+    @Test
+    public void testBiConsumerApply22() throws Exception {
+        Spy spy = new Spy();
+        eachPair(Consumers.apply22(spy::consume));
+        spy.checkConsumed(2, 2, 4, 4, 6, 6);
+    }
+
     @Test
     public void testBiConsumerBind1() throws Exception {
         Spy spy = new Spy();
@@ -20,8 +49,14 @@ public class ConsumersTest extends TestBase {
     }
 
     private void each(Consumer<Integer> consumer) {
-        for(int i = 1; i <= LENGTH; ++i) {
+        for (int i = 1; i <= LENGTH; ++i) {
             consumer.accept(i);
+        }
+    }
+
+    private void eachPair(BiConsumer<Integer, Integer> consumer) {
+        for (int i = 1; i <= LENGTH; ++i) {
+            consumer.accept(i * 2 - 1, i * 2);
         }
     }
 }
